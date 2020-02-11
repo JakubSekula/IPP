3457639e1f69a966c4670ce63a316db45892cd18
JakubSekula <kubasekula@seznam.cz>
2020-02-11 17:40:06 +0100

    Completed stdin and file in

--- parse.php
+++ parse.php
@@ -1,7 +1,6 @@
 <?php
 
-function getLine( $file ){
-    $line = fgets( $file );
+function returnLine( $line ){
     if ( ( $line ) || ( $line ) != 'EOF' ){
         return $line;
     } else {
@@ -9,17 +8,51 @@ function getLine( $file ){
     }
 }
 
-if ( $argc > 1 && $argv[1] == "--help" ){
+function getLine( $file ){
+    if ( $file ){
+        $line = fgets( $file );
+        return returnLine( $line );
+    } else {
+        $line = fgets( STDIN );
+        return returnLine( $line );
+    }
+}
+
+$arguments = array( 'help' => false, 'source' => '' );
+
+if ( $argc > 1 ){
+    foreach ( $argv as $arg ){
+        if ( $arg == "--help" ){
+            $arguments[ 'help' ] = true;
+        } elseif ( preg_match( '/^--source=(")*(\.)*([a-zA-Z]*(\/)*)*(")*$/', $arg ) ){
+            $arguments[ 'source' ] = substr( $arg, 9 );
+        }
+    }
+}
+
+if ( $arguments[ 'help' ] ){
     echo "Skript typu filtr (parse.php v jazyce PHP 7.4) nacte ze standardniho vstupu zdrojovy kod v IPP-code20, zkontroluje \nlexikalni a syntaktickou spravnost kodu a vypise na standardni vystup XML reprezentaci programu dle specifikace v sekci.\n";
     exit;
 }
 
-$file = fopen( "program", "r" ) or die( "Soubor nelze otevrit nebo neexistuje" );
-
+global $file;
+if ( $arguments[ 'source' ] != '' ){
+    if ( file_exists( $arguments[ 'source' ] ) ){
+        $file = fopen( $arguments[ 'source' ], "r" ) or exit( 11 );
+    } else {
+        exit( 11 );
+    }
+}
+/*
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 echo "<program language=\"IPPcode19\">\n";
-
-$FLine = fgets( $file );
+*/
+global $FLine;
+if ( $file ){
+    $FLine = fgets( $file );
+} else {
+    $FLine = fgets( STDIN );
+}
 
 $header = "/^[ ]*.IPPCODE20$/";
 
@@ -28,9 +61,9 @@ if ( !( preg_match( $header,$FLine ) ) ){
 }
 
 while ( ( $line = getLine( $file ) ) != NULL ){
-    //echo $line;
+    echo $line;
 }
 
-echo "</program>\n";
-fclose( $file );
+//echo "</program>\n";
+//fclose( $file );
 ?>
